8c10bd0fee4559b77a2f4e9e42f873300b4c2528
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2019-02-09 21:18:14 +0700

    refactor: set importer generator to be reusable

--- config/ambercat.config.js
+++ config/ambercat.config.js
@@ -1,3 +1,4 @@
+const { resolve } = require('path');
 const { resolve: resRoot } = require('app-root-path');
 
 module.exports = {
@@ -16,5 +17,17 @@ module.exports = {
     buildPrefix: 'server.build',
     devtool: false,
     entryFile: resRoot('src/entry-server.js'),
+  },
+  importer: {
+    postsFile: resRoot('tmp/posts.js'),
+    pagesFile: resRoot('tmp/pages.js'),
+  },
+  user: {
+    rootPath: resolve(process.cwd()),
+    configPath: resolve(process.cwd(), 'config'),
+    themePath: resolve(process.cwd(), 'theme'),
+    postsPath: resolve(process.cwd(), 'posts'),
+    pagesPath: resolve(process.cwd(), 'pages'),
+    publicPath: resolve(process.cwd(), 'public'),
   },
 };
--- scripts/post-resolver.js
+++ scripts/post-resolver.js
@@ -1,40 +1,6 @@
-const path = require('path');
-const { resolve: resRoot } = require('app-root-path');
-const glob = require('glob');
-const fs = require('fs');
+const { require: reqRoot } = require('app-root-path');
+const config = reqRoot('config/ambercat.config');
+const importerGenerator = reqRoot('lib/importer-generator');
 
-function ensureDirectoryExistence(filePath) {
-  const dirname = path.dirname(filePath);
-  if (!fs.existsSync(dirname)) {
-    ensureDirectoryExistence(dirname);
-    fs.mkdirSync(dirname);
-  }
-}
+importerGenerator(config.user.postsPath, 'md', config.importer.postsFile);
 
-function dynamicImportTemplate(name, folder = 'posts') {
-  return `
-  '${name}': () => import(/* webpackChunkName: '${folder}/${name}' */ '@/${folder}/${name}.md'),\n
-  `;
-}
-
-function mainTemplate(filenames) {
-  let template = 'export default {\n';
-  const body = filenames.reduce((acc, filename) => `${acc}${dynamicImportTemplate(filename)}`, '');
-  return `export default {\n  ${body}  \n};`;
-}
-
-function filenames() {
-  const postsPath = resRoot('posts');
-  const globPostsPath = `${postsPath}/*.md`;
-  return glob
-        .sync(globPostsPath)
-        .map(post => path.basename(post, '.md'));
-}
-
-function main() {
-  const targetFile = resRoot('tmp/posts.js');
-  ensureDirectoryExistence(targetFile);
-  fs.writeFileSync(targetFile, mainTemplate(filenames()), 'utf-8');
-}
-
-main();
