7a1dcc925dbb5ca5943c84c79a32ab6c22b07658
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2019-02-19 00:23:51 +0700

    polish: stylize 404 page

--- src/components/FourOhFourPage.vue
+++ src/components/FourOhFourPage.vue
@@ -1,3 +1,10 @@
 <template>
-  <p>404</p>
+  <div id="four-of-four-page text-lg text-grey-darkest">
+    <div class="bg-indigo h-1"/>
+    <div class="max-w-md mx-auto px-8">
+    <h1 class="text-5xl text-indigo-dark mt-16 mb-10">Uh Oh, You are lost!</h1>
+      <p class="text-xl mb-12">Seems like you are searching for something non existed.</p>
+      <p>Go back to <a href="/">homepage</a></p>
+    </div>
+  </div>
 </template>
