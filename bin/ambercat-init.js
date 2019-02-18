59e3014090031f726330e888b45eec5c6786f326
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2019-02-19 01:22:43 +0700

    debug: init command

--- bin/ambercat-init.js
+++ bin/ambercat-init.js
@@ -7,6 +7,10 @@ program.parse(process.argv);
 
 const targetDir = program.args[0];
 
+console.log('command root path:', resRoot('.'));
+console.log('source path:', resRoot('src'));
+console.log('target path:', path.join(targetDir, 'src/'));
+
 function ejectFiles(targetDir) {
   fsExtra.copySync(resRoot('src'), path.join(targetDir, 'src/'));
   fsExtra.copySync(resRoot('config/template.config.js'), path.join(targetDir, 'ambercat.config.js'));
