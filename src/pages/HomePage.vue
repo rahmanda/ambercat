aa62f6dcaa9bc64801de56215aa81a40d1136daf
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2019-02-18 19:50:15 +0700

    refactor: move post css to root

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="homepage" class="container mx-auto">
-    <h1 class="text-center text-3xl">Ambercat</h1>
-    <p class="text-center">A stupidly simple static site generator based on Vue.js</p>
+  <div id="homepage" class="bg-grey-lighter pt-4 pb-16 text-lg text-grey-darkest">
+    <div class="max-w-lg mx-auto px-8">
+      <h1 class="text-5xl text-indigo-dark mb-6">Ambercat</h1>
+      <p class="mb-6">A stupidly simple static site generator based on Vue.js</p>
+    </div>
   </div>
 </template>
--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <main role="main" class="post font-sans">
+  <main role="main" class="post">
     <h1>{{ postData.title }}</h1>
     <time class="text-base mb-8 text-grey-dark block"
           :datetime="postData.date">
@@ -41,89 +41,3 @@ export default {
   },
 }
 </script>
-
-<style lang="scss">
-.post {
-  @apply max-w-md mx-auto leading-normal px-8 text-lg pb-10;
-
-  h1 {
-    @apply mt-16 mb-8 text-5xl text-indigo-dark leading-tight;
-  }
-
-  h2 {
-    @apply my-8 text-3xl text-black leading-tight;
-  }
-
-  h3 {
-    @apply my-6 text-2xl text-black leading-tight;
-  }
-
-  h4, h5, h6 {
-    @apply my-4 text-xl text-black leading-tight;
-  }
-
-  hr {
-    @apply my-12 mx-16 border-solid border-b border-grey;
-  }
-
-  blockquote {
-    @apply text-center text-black px-4 text-xl my-10 font-semibold;
-  }
-
-  p {
-    @apply text-grey-darkest mb-6;
-  }
-
-  p, ol, ul {
-    code {
-      @apply text-xs bg-grey-light px-2 py-1 rounded;
-    }
-  }
-
-  pre {
-    @apply bg-black text-grey-light px-8 py-4 overflow-y-auto text-sm my-10 -mx-8;
-  }
-
-  @screen sm {
-    pre {
-      @apply rounded-none;
-    }
-  }
-
-  ol, ul {
-    @apply text-grey-darkest my-10;
-  }
-
-  table {
-    @apply text-grey-darkest my-10 -mx-8;
-
-    th, td {
-      @apply px-8 py-2;
-    }
-
-    th {
-      @apply font-semibold;
-    }
-
-    tr:nth-child(even), thead {
-      @apply bg-grey-lighter;
-    }
-  }
-
-  @screen md {
-    pre {
-      @apply rounded-lg;
-    }
-  }
-
-  a:not(.anchorjs-link) {
-    @apply font-semibold text-black relative no-underline;
-    background-image: -webkit-gradient(linear,left top,left bottom,color-stop(70%,transparent),color-stop(70%,rgba(101,125,225,.4)));
-    background-image: linear-gradient(180deg,transparent 70%,rgba(101,125,225,.4) 0);
-  }
-
-  .anchorjs-link {
-    @apply text-indigo;
-  }
-}
-</style>
