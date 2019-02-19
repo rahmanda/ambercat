87f6cbad4a9bcb53d1af37e7414b9921bb11f5b9
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2019-02-19 21:13:33 +0700

    refactor(asset-injector): append string instead of getting a whole string from user

--- config/default.config.js
+++ config/default.config.js
@@ -12,14 +12,15 @@ module.exports = {
     { filename: 'index', title: 'Ambercat' },
     { filename: '404', title: '404' },
   ],
-  // @param {string} tag - main compiled script tag(s)
   // @param {string} assetType - contains 'js' or 'css'
   // @param {string} pageType - contains 'page' (regular static page) or 'post'
-  assetInjector(tag, assetType, pageType) {
-    return tag;
+  // @return {string}
+  assetInjector(assetType, pageType) {
+    return '';
   },
   // @param {object} config - default webpack config
   // @param {boolean} isServer - webpack config type
+  // @return {object}
   configureWebpack(config, isServer) {
     return {};
   },
