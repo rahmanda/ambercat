625178d8e38d4d3950c0844378ed483db8ba2bcb
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2019-02-19 11:16:52 +0700

    polish: create spinner log on init script

--- bin/ambercat-init.js
+++ bin/ambercat-init.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { resolve: resRoot, setPath } = require('app-root-path');
 const program = require('commander');
 const fsExtra = require('fs-extra');
+const logger = require('@vue/cli-shared-utils');
 
 setPath(path.resolve(__dirname, '..'));
 
@@ -10,9 +11,11 @@ program.parse(process.argv);
 const targetDir = program.args[0];
 
 function ejectFiles(targetDir) {
+  logger.logWithSpinner('Initialize project...');
   fsExtra.copySync(resRoot('src'), path.join(targetDir, 'src/'));
   fsExtra.copySync(resRoot('config/default.config.js'), path.join(targetDir, 'ambercat.config.js'));
   fsExtra.copySync(resRoot('.gitignore.template'), path.join(targetDir, '.gitignore'));
+  logger.stopSpinner();
 }
 
 if (targetDir) {
