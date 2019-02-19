7b4a8b55d1519af9d7268f0e162f9b211256d05c
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2019-02-20 00:33:05 +0700

    style: enlarge reading space

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="homepage" class="text-lg text-grey-darkest">
-    <div class="max-w-md mx-auto px-8 pt-16 pb-8">
+    <div class="max-w-md mx-auto px-6 pt-16 pb-8">
       <h1 class="text-5xl text-indigo-dark mt-0 mb-4">Ambercat</h1>
       <p class="text-xl mb-0 font-semibold text-grey-dark">A stupidly simple static site generator based on Vue.js and Tailwind CSS</p>
     </div>
-    <div class="max-w-md mx-auto px-8 py-6">
+    <div class="max-w-md mx-auto px-6 py-6">
       <div v-for="post in data.posts"
            class="mb-12">
         <div class="text-sm text-grey-darker">
