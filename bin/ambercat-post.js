7b21b4e73febda5d0f4ef94781d2b156801cf1e9
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2019-02-19 14:59:15 +0700

    fix(cli): resolve wrong path reference

--- bin/ambercat-post.js
+++ bin/ambercat-post.js
@@ -1,13 +1,14 @@
 const path = require('path');
-const fs = require('fs');
+
 const { resolve: resRoot, require: reqRoot, setPath } = require('app-root-path');
+setPath(path.resolve(__dirname, '..'));
+
+const fs = require('fs');
 const config = reqRoot('config');
 const program = require('commander');
 const logger = require('@vue/cli-shared-utils');
 const moment = require('moment');
 const squirrelly = require('squirrelly');
-
-setPath(path.resolve(__dirname, '..'));
 
 program.parse(process.argv);
 
@@ -24,7 +25,7 @@ function createNewPost(title) {
   const filename = `${date}-${title.toLowerCase().replace(/\s/g, '-')}.md`;
   const filePath = path.join(config.postPath, filename);
   logger.logWithSpinner(`Creating ${filePath}...`);
-  const template = fs.readFileSync(resRoot('config/post.template'), 'utf-8');
+  const template = fs.readFileSync(resRoot('post.template'), 'utf-8');
   const content = squirrelly.Render(template, { title });
   fs.writeFileSync(filePath, content, 'utf-8');
   logger.stopSpinner();
