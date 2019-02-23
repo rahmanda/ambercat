e8abe4e27d4aef55d3e3d72cf7153df784acc2be
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2019-02-23 17:40:49 +0700

    refactor: split compiler between post and page

--- scripts/static-compiler.js
+++ scripts/static-compiler.js
@@ -1,27 +1,31 @@
 const { require: reqRoot } = require('app-root-path');
 const config = reqRoot('config');
-const htmlCompiler = reqRoot('lib/html-compiler');
+const { compilePage, compileHomepage } = reqRoot('lib/page-compiler');
+const compilePosts = reqRoot('lib/post-compiler');
 const logger = require('@vue/cli-shared-utils');
-const { getRecentPosts } = reqRoot('lib/helpers/post');
 
 function compileStatic() {
   logger.logWithSpinner('Compiling static files...');
   let processes = [];
   processes.push(
-    htmlCompiler.compileDir(config.postPath, config.postExt, config.client.buildPath, 'post')
+    compilePosts(config.postPath, config.client.buildPath)
   );
-  const recentPosts = getRecentPosts(config.postPath, config.postExt, config.numOfRecentPosts);
   config.staticPages.forEach(({ filename, title }) => {
     const context = {
       url: `/${filename}.html`,
       data: {
         title,
-        posts: recentPosts,
       },
     };
-    processes.push(
-      htmlCompiler.compileFile(context, filename, config.client.buildPath, 'page')
-    );
+    if (filename === 'index') {
+      processes.push(
+        compileHomepage(config.postPath, config.client.buildPath, config.numOfRecentPosts, context)
+      );
+    } else {
+      processes.push(
+        compilePage(config.client.buildPath, context)
+      );
+    }
   });
   return Promise.all(processes)
     .then(() => {
