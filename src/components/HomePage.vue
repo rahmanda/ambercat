26cbb17762b5ae7706a6ac7cc6eefe1a7c582af4
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2019-03-06 19:33:19 +0700

    style: enlarge reading space

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,10 +1,10 @@
 <template>
   <main role="main" id="homepage" class="text-lg text-grey-darkest">
-    <div class="max-w-md mx-auto px-6 py-12">
+    <div class="max-w-md mx-auto px-4 py-12">
       <h1 class="text-5xl text-indigo-dark mt-0 mb-4">{{ data.title }}</h1>
       <p class="text-xl mb-0 font-semibold text-grey-dark">{{ data.description }}</p>
     </div>
-    <div class="max-w-md mx-auto px-6 py-6">
+    <div class="max-w-md mx-auto px-4 py-6">
       <div v-for="post in data.posts"
            class="mb-10">
         <div class="text-sm text-grey-darker">
